c4fc39e0b9c3b4a7156d7931b7b290d4528247eb
Michael Aufreiter <michael@substance.io>
2013-08-15 17:10:01 +0200

    Making use of the new DOM constructor.

--- index.js
+++ index.js
@@ -3,5 +3,7 @@
 var Application = require("./src/application");
 Application.View = require("./src/view");
 Application.Controller = require("./src/controller");
+Application.ElementRenderer = require("./src/renderers/element_renderer");
+Application.$$ = Application.ElementRenderer.$$;
 
 module.exports = Application;
