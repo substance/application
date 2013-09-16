0b02776026d347d0dec35093e1910a0f4b7e8005
Michael Aufreiter <michael@substance.io>
2013-09-16 15:32:42 +0200

    Substance.Application Router birthday.

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 
 var Application = require("./src/application");
 Application.View = require("./src/view");
+Application.Router = require("./src/router");
 Application.Controller = require("./src/controller");
 Application.ElementRenderer = require("./src/renderers/element_renderer");
 Application.$$ = Application.ElementRenderer.$$;
--- src/application.js
+++ src/application.js
@@ -1,7 +1,9 @@
 "use strict";
 
 var View = require("./view");
+var Router = require("./router");
 var util = require("substance-util");
+var _ = require("underscore");
 
 // Substance.Application
 // ==========================================================================
@@ -10,24 +12,32 @@ var util = require("substance-util");
 
 var Application = function(config) {
   View.call(this);
-
   this.config = config;
-
-
-
 };
 
 Application.Prototype = function() {
+  // Init router
+  // ----------
+
+  this.initRouter = function() {
+    this.router = new Router();
+
+    _.each(Lens.routes, function(route) {
+      this.router.route(route.route, route.name, _.bind(this.controller[route.command], this.controller));
+    }, this);
+
+    Router.history.start();
+  };
 
   // Start Application
   // ----------
   //
 
   this.start = function() {
-
+    this.initRouter();
     this.$el = $('body');
     this.el = this.$el[0];
-
+    this.render();
   };
 
 };
