f7a757047c6c25d1348c030d49dadbfff48c5b5f
Oliver Buchtala <oliver.buchtala@googlemail.com>
2013-11-14 17:38:35 +0100

    First iteration on updating the window URL after state transitions.

--- src/application.js
+++ src/application.js
@@ -1,8 +1,9 @@
 "use strict";
 
 var View = require("./view");
-var util = require("substance-util");
+//var util = require("substance-util");
 var Controller = require("./controller");
+var _ = require("underscore");
 
 // Substance.Application
 // ==========================================================================
@@ -12,6 +13,8 @@ var Controller = require("./controller");
 var Application = function(config) {
   View.call(this);
   this.config = config;
+
+  this.__controller__ = null;
 };
 
 Application.Prototype = function() {
@@ -44,7 +47,7 @@ Application.Prototype = function() {
     cb = cb || function(error) {
       if (error) throw new Error(error);
     };
-    this.controller.switchState(appState, cb);
+    this.controller.__switchState__(appState, cb);
   };
 
   this.extractStateFromURL = function(locationSearch) {
@@ -85,9 +88,62 @@ Application.Prototype = function() {
     return state;
   };
 
+  this.getState = function() {
+    var appState = [];
+    var controller = this.controller;
+    while(controller) {
+      appState.push(controller.state);
+      controller = controller.childController;
+    }
+    return appState;
+  };
+
+  this.updateURL = function() {
+    console.log("Application.updateURL()...");
+    var appState = this.getState();
+    var stateIds = [];
+    var queryParams = [];
+    for (var i = 0; i < appState.length; i++) {
+      var s = appState[i];
+      if (!s) continue;
+      stateIds.push(s.id);
+      for (var key in s) {
+        var val = s[key];
+        if (key === "id" || key === "__id__") {
+          continue
+        }
+        // Note: currently only String variables are allowed as state variables
+        if (!_.isString(val)) {
+          console.error("Only String state variables are allowed");
+          continue;
+        }
+        queryParams.push(i+"."+key+"="+val);
+      }
+    }
+
+    queryParams.unshift("state="+stateIds.join("."));
+    var queryString = "?" + queryParams.join(";");
+
+    window.history.pushState(appState, "", "/"+queryString);
+  };
+
+  // Called by a sub controller when a sub-state has been changed
+  this.stateChanged = function() {
+    this.updateURL();
+  };
 };
 
 Application.Prototype.prototype = View.prototype;
 Application.prototype = new Application.Prototype();
+
+Object.defineProperty(Application.prototype, "controller", {
+  set: function(controller) {
+    controller.setChangeListener(this);
+    this.__controller__ = controller;
+  },
+  get: function() {
+    return this.__controller__;
+  }
+});
 
 module.exports = Application;
--- src/controller.js
+++ src/controller.js
@@ -9,11 +9,15 @@ var _ = require("underscore");
 // Application Controller abstraction suggesting strict MVC
 
 var Controller = function() {
+
+  // an object that has a method 'stateChanged()'
+  this.changeListener = null;
+
   // the state is represented by a unique name
   this.state = null;
 
   // Each controller can have a single (active) child controller
-  this.childController = null;
+  this.__childController__ = null;
 };
 
 Controller.Prototype = function() {
@@ -27,8 +31,8 @@ Controller.Prototype = function() {
   // A built-in transition function which is the opposite to `initialize`
   // ----
   this.dispose = function() {
-    if (this.childController) this.childController.dispose();
-    this.childController = null;
+    if (this.__childController__) this.__childController__.dispose();
+    this.__childController__ = null;
     this.state = null;
   };
 
@@ -37,6 +41,19 @@ Controller.Prototype = function() {
   };
 
   this.switchState = function(state, cb) {
+    var self = this;
+    cb = cb || function(err) {
+      if (err) throw new Error(err);
+    };
+    this.__switchState__(state, function(error) {
+      if (error) return cb(error);
+      if (self.changeListener) self.changeListener.stateChanged();
+      cb(null);
+    });
+  };
+
+  this.__switchState__ = function(state, cb) {
+    console.log("Controller.switchState", state);
     var self = this;
 
     cb = cb || function(err) {
@@ -49,38 +66,42 @@ Controller.Prototype = function() {
 
     var _state = state.shift();
 
-    var _transition = function() {
-      self.transition(_state, function(error, skipped) {
-        if (error) return cb(error);
-        if (!skipped) {
-          self.state = _state;
-          // recurse
-          if (self.childController) {
-            if (state.length > 0) {
-              self.childController.switchState(state, function(error) {
-                if (error) return cb(error);
-                self.afterTransition();
-                cb(null);
-              });
-            }
-            else if (self.childController.AUTO_INIT) {
-              self.childController.initialize(null, function(error){
-                if (error) return cb(error);
-                self.afterTransition();
-                cb(null);
-              });
-            }
-            else {
-              return cb("Unsufficient state data provided! Child controller needs a transition!");
-            }
-          }
+    var _afterTransition = function() {
+      var oldState = self.state;
+      self.state = _state;
+      self.afterTransition(oldState);
+      cb(null);
+    };
 
-          else {
-            self.afterTransition();
-            cb(null);
+    var _transition = function() {
+      console.log("Transition to", _state);
+      self.transition(_state, function(error) {
+        if (error) return cb(error);
+
+        // The transition has been done in this level, i.e., child controllers
+        // might have been created.
+        // If a child controller exists we recurse into the next level.
+        // After that the controller gets triggered about the finished transition.
+
+        if (self.childController) {
+          if (state.length > 0) {
+            self.childController.__switchState__(state, function(error) {
+              if (error) return cb(error);
+              _afterTransition();
+            });
           }
-        } else {
-          cb(null);
+          else if (self.childController.AUTO_INIT) {
+            self.childController.initialize(null, function(error){
+              if (error) return cb(error);
+              _afterTransition();
+            });
+          }
+          else {
+            return cb("Unsufficient state data provided! Child controller needs a transition!");
+          }
+        }
+        else {
+          _afterTransition();
         }
       });
     };
@@ -88,6 +109,7 @@ Controller.Prototype = function() {
     // If no transitions are given we still can use dispose/initialize
     // to reach the new state
     if (!this.state) {
+      console.log("Initializing...", _state);
       this.initialize(_state, function(error) {
         if (error) return cb(error);
         self.state = {id: "initialized"};
@@ -100,7 +122,48 @@ Controller.Prototype = function() {
 
   this.afterTransition = function() {};
 
+  this.setChildController = function(childController) {
+    if (this.__childController__ && this.__childController__.state) {
+      console.error("The child controller has not been disposed. Call 'disposeChildController()' first. However, let me do this for you once more...");
+      this.__childController__.dispose();
+    }
+    if (!childController) {
+      return;
+    }
+    if (!this.changeListener) {
+      // We need to establish a consistent connection between (Sub-)Controllers and the Application
+      // instance to be able to notify the app about changes in the sub state
+      // For now, I decided to propagate the application when sub-controllers are attached
+      // to parent controllers.
+      // This makes sense w.r.t the current mechanism of state transitions which
+      // works from top to down. I.e., a parent controller is either the top-level controller
+      // or itself a child of an already attached controller.
+      // A global/singleton Application instance would be possible, however I reject introducing
+      // such an evil thing. It breaks modularity and makes testing harder.
+      // Alternatively one could require this to be given when constructing Controllers,
+      // however, this would require to change all constructors.
+      console.error("This controller does not have a changeListener attached, so the child controller will not trigger corresponding application state changes.");
+    } else {
+      childController.changeListener = this.changeListener;
+    }
+    this.__childController__ = childController;
+  };
+
+  this.disposeChildController = function() {
+    if (this.__childController__) {
+      this.__childController__.dispose();
+      this.__childController__ = null;
+    }
+  };
+
+  this.setChangeListener = function(changeListener) {
+    this.changeListener = changeListener;
+  };
+
 };
+
+Controller.Prototype.prototype = util.Events;
+Controller.prototype = new Controller.Prototype();
 
 Controller.State = function(id) {
   if (_.isString(id)) {
@@ -124,8 +187,13 @@ Object.defineProperty(Controller.State.prototype, "id", {
   }
 });
 
-// Setup prototype chain
-Controller.Prototype.prototype = util.Events;
-Controller.prototype = new Controller.Prototype();
+Object.defineProperty(Controller.prototype, "childController", {
+  set: function(childController) {
+    this.setChildController(childController);
+  },
+  get: function() {
+    return this.__childController__;
+  }
+});
 
 module.exports = Controller;
